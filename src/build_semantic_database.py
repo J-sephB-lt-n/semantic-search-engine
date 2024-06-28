cfdff9e386dc9eaccd14a00e810b7536b0005b8c
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-06-28 22:37:42 +0200

    feat(build_semantic_database.py): finished v1

--- src/build_semantic_database.py
+++ src/build_semantic_database.py
@@ -1,5 +1,7 @@
+import pathlib
 import pickle
 
+import pymilvus
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
@@ -12,4 +14,39 @@ embed_model = SentenceTransformer(
     "Alibaba-NLP/gte-large-en-v1.5", trust_remote_code=True
 )
 
-embed_model.encode(chunks[69].text)
+pathlib.Path("databases/embeddings.db").unlink(missing_ok=True)
+milvus_client = pymilvus.MilvusClient("databases/embeddings.db")
+
+milvus_client.create_collection(
+    collection_name="lookup_docs",
+    dimension=1024,
+    schema=pymilvus.CollectionSchema(
+        fields=[
+            pymilvus.FieldSchema(
+                name="id",
+                dtype=pymilvus.DataType.INT64,
+                is_primary=True,
+            ),
+            pymilvus.FieldSchema(
+                name="embedding", dtype=pymilvus.DataType.FLOAT_VECTOR, dim=1024
+            ),
+            pymilvus.FieldSchema(
+                name="source_name", dtype=pymilvus.DataType.VARCHAR, max_length=999
+            ),
+        ]
+    ),
+)
+
+for idx, chunk in tqdm(enumerate(chunks)):
+    milvus_client.insert(
+        collection_name="lookup_docs",
+        data=[
+            {
+                "id": idx,
+                "embedding": embed_model.encode(chunk.text),
+                "source_name": chunk.metadata.source_name,
+            }
+        ],
+    )
+
+milvus_client.close()
