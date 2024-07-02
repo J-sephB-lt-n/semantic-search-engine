eef55888444b21219343d02dc7064ea142404b16
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-02 22:30:16 +0200

    feat(ixed_size_chunks.py): finished function, but not handling for overlap

---
+++ chunking_functions/fixed_size_chunks.py
@@ -0,0 +1,41 @@
+"""
+Defines function for splitting text into chunks of fixed size (with optional overlap) 
+"""
+
+from objects import Chunk
+
+
+def chunk_by_fixed_size(
+    input_text: str, chunk_nchar: int, overlap_nchar: int = 0
+) -> tuple[Chunk, ...]:
+    """Chunks (splits) `input_text` into chunks of size `chunk_nchar`, optionally with
+    content overlap of `overlap_nchar` characters between the chunks
+
+    Returns:
+        tuple[Chunk, ...]: a tuple of Chunk objects
+    """
+    chunks: list[Chunk] = []
+    current_index: int = 0
+    while True:
+        chunk_end_index = current_index + chunk_nchar - 1
+        if chunk_end_index <= len(input_text):
+            chunks.append(
+                Chunk(
+                    text=input_text[current_index : chunk_end_index + 1],
+                    start_index=current_index,
+                    end_index=chunk_end_index,
+                )
+            )
+            current_index += chunk_nchar
+        else:
+            chunk_end_index = len(input_text) - 1
+            chunks.append(
+                Chunk(
+                    text=input_text[current_index : chunk_end_index + 1],
+                    start_index=current_index,
+                    end_index=chunk_end_index,
+                )
+            )
+            break
+
+    return tuple(chunks)
--- objects.py
+++ objects.py
@@ -1,3 +1,3 @@
 from collections import namedtuple
 
-Chunk = namedtuple("Chunk", "text start_index end_index metadata")
+Chunk = namedtuple("Chunk", "text start_index end_index")
