a778d49d5e1b34502848a573d7069d53510a4f17
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-03 21:47:22 +0200

    refactor(objects.py): redefined chunk interface

--- objects.py
+++ objects.py
@@ -1,3 +1,7 @@
 from collections import namedtuple
 
-Chunk = namedtuple("Chunk", "text start_index end_index")
+Chunk = namedtuple("Chunk", "text start_index end_index chunk_num metadata")
+
+ChunkMetadata = namedtuple(
+    "ChunkMetadata", "source_doc chunk_algorithm chunk_algorithm_params"
+)
