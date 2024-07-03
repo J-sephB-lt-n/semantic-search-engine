ec11198a6f077509f810f4ed863d6b133bb84128
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-03 21:47:46 +0200

    test(tests/): updated tests for new chunk interface

--- tests/chunking_functions/test_chunk_by_paragraph.py
+++ tests/chunking_functions/test_chunk_by_paragraph.py
@@ -7,13 +7,21 @@ def test_chunk_by_paragraph():
     # Test case 1: Regular input with multiple paragraphs
     input_text = "\n\n    sdf\n    SDFSDKJFNSDNFDSF\n\n    fsdniunfdsiudfnsufnsdunfsudinf\n    fuids8392jbke\n\n\n23\n\n   \n\n\n\n  123\n\n    "
     assert chunk_by_paragraph(input_text, min_chunk_nchar=3) == (
-        Chunk(text="sdf\n    SDFSDKJFNSDNFDSF", start_index=6, end_index=30),
+        Chunk(
+            text="sdf\n    SDFSDKJFNSDNFDSF",
+            start_index=6,
+            end_index=30,
+            chunk_num=0,
+            metadata=None,
+        ),
         Chunk(
             text="fsdniunfdsiudfnsufnsdunfsudinf\n    fuids8392jbke",
             start_index=36,
             end_index=84,
+            chunk_num=1,
+            metadata=None,
         ),
-        Chunk(text="123", start_index=100, end_index=103),
+        Chunk(text="123", start_index=100, end_index=103, chunk_num=2, metadata=None),
     )
 
 
--- tests/chunking_functions/test_fixed_size_chunks.py
+++ tests/chunking_functions/test_fixed_size_chunks.py
@@ -7,18 +7,36 @@ def test_chunk_by_paragraph():
     # Test case 1: Regular input with multiple paragraphs
     input_text = "abcdefghijklmnop\nqrst\n\nuvwxyz"
     assert chunk_by_fixed_size(input_text, chunk_nchar=8) == (
-        Chunk(text="abcdefgh", start_index=0, end_index=7),
-        Chunk(text="ijklmnop", start_index=8, end_index=15),
-        Chunk(text="\nqrst\n\nu", start_index=16, end_index=23),
-        Chunk(text="vwxyz", start_index=24, end_index=28),
+        Chunk(text="abcdefgh", start_index=0, end_index=7, chunk_num=0, metadata=None),
+        Chunk(text="ijklmnop", start_index=8, end_index=15, chunk_num=1, metadata=None),
+        Chunk(
+            text="\nqrst\n\nu", start_index=16, end_index=23, chunk_num=2, metadata=None
+        ),
+        Chunk(text="vwxyz", start_index=24, end_index=28, chunk_num=3, metadata=None),
     )
 
     assert chunk_by_fixed_size(input_text, chunk_nchar=10, overlap_nchar=4) == (
-        Chunk(text="abcdefghij", start_index=0, end_index=9),
-        Chunk(text="ghijklmnop", start_index=6, end_index=15),
-        Chunk(text="mnop\nqrst\n", start_index=12, end_index=21),
-        Chunk(text="rst\n\nuvwxy", start_index=18, end_index=27),
-        Chunk(text="vwxyz", start_index=24, end_index=28),
+        Chunk(
+            text="abcdefghij", start_index=0, end_index=9, chunk_num=0, metadata=None
+        ),
+        Chunk(
+            text="ghijklmnop", start_index=6, end_index=15, chunk_num=1, metadata=None
+        ),
+        Chunk(
+            text="mnop\nqrst\n",
+            start_index=12,
+            end_index=21,
+            chunk_num=2,
+            metadata=None,
+        ),
+        Chunk(
+            text="rst\n\nuvwxy",
+            start_index=18,
+            end_index=27,
+            chunk_num=3,
+            metadata=None,
+        ),
+        Chunk(text="vwxyz", start_index=24, end_index=28, chunk_num=4, metadata=None),
     )
 
 
