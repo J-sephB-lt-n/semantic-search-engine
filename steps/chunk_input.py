6caec0f5c72faec795582bcba3c93777201f3094
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-07-03 21:54:17 +0200

    feat(steps/chunk_input.py): Finished chunking script v1

--- steps/chunk_input.py
+++ steps/chunk_input.py
@@ -1,13 +1,17 @@
 """
-Chunks every text file in /input_docs_txt/ using various different
-approaches, writing the results to /chunked_input/ as a .pickle file
+Combines all text files in /input_docs_txt/ into a single document, then chunks 
+this master document using various different approaches, writing the 
+results to /chunked_input/ as .pickle files
 """
 
+import json
+import itertools
 import pathlib
 import pickle
 from typing import Final
 
 from chunking_functions import chunk_by_fixed_size, chunk_by_paragraph
+from objects import Chunk, ChunkMetadata
 
 INPUT_DIRPATH: Final[str] = "./input_docs_txt"
 OUTPUT_DIRPATH: Final[str] = "./chunked_input"
@@ -17,24 +21,42 @@ for path in pathlib.Path(OUTPUT_DIRPATH).iterdir():
         path.unlink()
         print(f"Deleted file [{path}]")
 
+input_docs = {}
 for path in pathlib.Path(INPUT_DIRPATH).iterdir():
     if path.is_file() and path.suffix == ".txt":
         with open(path, "r", encoding="utf-8") as file:
-            input_text = file.read()
-        for chunker in (
-            {"func": chunk_by_fixed_size, "params": {"chunk_nchar": 100}},
-            {
-                "func": chunk_by_fixed_size,
-                "params": {"chunk_nchar": 100, "overlap_nchar": 20},
-            },
-        ):
-            chunks = chunker["func"](input_text, **chunker["params"])
-            chunker_desc = (
-                chunker["func"].__name__
-                + "-"
-                + "-".join([f"{k}{v}" for k, v in chunker["params"].items()])
+            input_docs[path.name] = file.read()
+
+for chunker in (
+    {"func": chunk_by_fixed_size, "params": {"chunk_nchar": 100, "overlap_nchar": 0}},
+    {
+        "func": chunk_by_fixed_size,
+        "params": {"chunk_nchar": 100, "overlap_nchar": 20},
+    },
+):
+    chunks: list[Chunk] = []
+    for doc_name, text in input_docs.items():
+        chunk_counter = itertools.count()
+        for chunk in chunker["func"](text, **chunker["params"]):
+            chunks.append(
+                Chunk(
+                    text=chunk.text,
+                    start_index=chunk.start_index,
+                    end_index=chunk.end_index,
+                    chunk_num=chunk.chunk_num,
+                    metadata=ChunkMetadata(
+                        source_doc=doc_name,
+                        chunk_algorithm=chunker["func"].__name__,
+                        chunk_algorithm_params=json.dumps(chunker["params"]),
+                    ),
+                )
             )
-            output_filepath = f"{OUTPUT_DIRPATH}/{path.stem}-{chunker_desc}.pickle"
-            with open(output_filepath, "wb") as file:
-                pickle.dump(chunks, file, protocol=pickle.HIGHEST_PROTOCOL)
-                print(f"exported file [{output_filepath}")
+    chunk_algorithm_desc = (
+        chunker["func"].__name__
+        + "-"
+        + "-".join([f"{k}{v}" for k, v in chunker["params"].items()])
+    )
+    output_filepath = f"{OUTPUT_DIRPATH}/{chunk_algorithm_desc}.pickle"
+    with open(output_filepath, "wb") as file:
+        pickle.dump(chunks, file, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"exported file [{output_filepath}")
